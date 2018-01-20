e3f714ac0d1dcf7aeb69214c01e6979dd9a998db
meganriley <meganriley@users.noreply.github.com>
2018-01-20 12:52:23 -0600

    Create quizstore.js

---
+++ quizstore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+      question: 'Dumbledore has a scar above his left knee that is the perfect map of what?',
+      answers: [
+        'Hogsmeade Village',
+        'Hogwarts',
+        'Diagon Alley',
+        'The London Underground'
+        ],
+      correctAnswer: 'The London Underground',
+      icon: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-10/16/15/enhanced/webdr03/original-6320-1445023846-3.png?downsize=715:*&output-format=auto&output-quality=auto',
+      alt: 'Albus Dumbledore'
+    },
+    {
+      question: 'What did Ginny Weasley name her pygmy puff?',
+      answers: [
+        'Harold',
+        'Rupert',
+        'Arnold',
+        'She never named it.'
+        ],
+        correctAnswer: 'Arnold',
+        icon: 'https://i.pinimg.com/236x/af/b9/1c/afb91cb2d595d777d06486ae03006196.jpg',
+        alt: 'Arnold on Ginny\'s shoulder'
+    },
+    {
+      question: 'What is a Mudblood?',
+      answers: [
+        'Someone born into a wizarding family but doesn\'t have any magical ability',
+        'One of the 4 houses at Hogwarts',
+        'One of Hagrid\'s favorite beasts in Care of Magical Creatures',
+        'A derogatory term for a Muggle-born wizard or witch'
+        ],
+        correctAnswer: 'A derogatory term for a Muggle-born wizard or witch',
+        icon: 'https://images.8tracks.com/cover/i/009/631/991/IMG_20151102_091444-3897.jpg?rect=0,0,451,451&q=98&fm=jpg&fit=max',
+        alt: 'Mudblood scar'
+    },
+    {
+      question: 'What is the name of the magazine Luna Lovegood reads?',
+      answers: [
+        'The Daily Prophet',
+        'The Howler',
+        'The Quibbler',
+        'The Remembrall'
+        ],
+      correctAnswer: 'The Quibbler',
+      icon: 'https://images-na.ssl-images-amazon.com/images/I/61caukjI0kL._SX463_.jpg',
+      alt: 'The Quibbler magazine'
+    },
+    {
+      question: 'Which of these is NOT a Hogwarts House founder?',
+      answers: [
+        'Sirius Slytherin',
+        'Godric Griffyndor',
+        'Rowena Ravenclaw',
+        'Helga Hufflepuff'
+        ],
+      correctAnswer: 'Sirius Slytherin',
+      icon: 'https://i.ytimg.com/vi/YHHLWJA6io8/maxresdefault.jpg',
+      alt: 'The four Hogwarts founders'
+    },
+    {
+      question: 'What is the correct pronunciation of the Levitation Charm?',
+      answers: [
+        'Ridd-<strong>IK</strong>-u-<strong>lus</strong>',
+        'Wing-<strong>GAR</strong>-dium Levi-<strong>O</strong>-sa',
+        'Ex-<strong>PELL</strong>-i-ar-<strong>MUS</strong>',
+        'Wing-<strong>GAR</strong>-dium Levi-o-<strong>SA</strong>'
+        ],
+      correctAnswer: 'Wing-<strong>GAR</strong>-dium Levi-<strong>O</strong>-sa',
+      icon: 'https://vignette.wikia.nocookie.net/harrypotter/images/4/43/Wingardium_leviosa.jpg/revision/latest/scale-to-width-down/515?cb=20100621142528',
+      alt: 'Hermione using Levitation Charm'
+    },
+    {
+      question: 'What is Hermione Granger\'s Patronus?',
+      answers: [
+      'Stag',
+      'Otter',
+      'Rabbit',
+      'Doe'
+      ],
+      correctAnswer: 'Otter',
+      icon: 'https://vignette.wikia.nocookie.net/harrypotter/images/f/f3/Hermione_Otter_Patronus.png/revision/latest?cb=20170109003508',
+      alt: 'Otter Patronus'
+    },
+    {
+      question: 'Which one of these was not a Marauder?',
+      answers: [
+        'Moody',
+        'Padfoot',
+        'Prongs',
+        'Wormtail'
+      ],
+      correctAnswer: 'Moody',
+      icon: 'https://secure.img1-fg.wfcdn.com/im/46113240/resize-h600-w600%5Ecompr-r85/4049/40493779/Harry+Potter+%27Marauders+Map%27+Graphic+Art+Print.jpg',
+      alt: 'The Marauder\'s Map'
+    },
+    {
+      question: 'What do Herminone Granger\'s parents do for a living?',
+      answers: [
+      'Doctors',
+      'Lawyers',
+      'Accountants',
+      'Dentists'
+      ],
+      correctAnswer: 'Dentists',
+      icon: 'https://vignette.wikia.nocookie.net/harrypotter/images/e/e8/Granger_family.jpg/revision/latest?cb=20150118093510',
+      alt: 'Hermione Granger\'s family'
+    },
+    {
+      question: 'How many unforgiveable curses are there?',
+      answers: [
+        'One',
+        'Two',
+        'Three',
+        'Four'
+    ],
+    correctAnswer: 'Three',
+    icon: 'https://pm1.narvii.com/6108/c8c7f2bb785df166479be62ea73ed15cb55cb6f3_hq.jpg',
+    alt: 'The Three Unforgiveable Curses'
+    }
+];
